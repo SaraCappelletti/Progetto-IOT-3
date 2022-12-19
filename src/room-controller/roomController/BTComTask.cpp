9e3202de07734994097c24641f05e3d19f8b5d1e
Leonardo Randacio <leonardo.randacio@gmail.com>
2022-12-19 15:28:24 +0100

    continued BTComTask. only internal logic after message is recieved missing

--- src/room-controller/roomController/BTComTask.cpp
+++ src/room-controller/roomController/BTComTask.cpp
@@ -2,16 +2,16 @@
 #include "SoftwareSerial.h"
 #include <Arduino.h>
 
-BTComTask::BTComTask(int rxPin, int txPin){
-  SoftwareSerial channel(rxPin, txPin);
-}
+BTComTask::BTComTask(int rxPin, int txPin, ServoMotor* servoMotor, Led* led) : 
+  rxPin(rxPin), txPin(txPin), servoMotor(servoMotor), led(led) {}
   
 void BTComTask::init(int period){
   Task::init(period);
   channel.begin(9600);
+  SoftwareSerial channel(rxPin, txPin);
 }
   
 void BTComTask::tick(){
   char msg = (char) channel.read();
-  //TODO controllare il messaggio ricevuto dal BT e agire di conseguenza
+  //TODO controllare il messaggio ricevuto dal BT e muovere il servo/accendere la luce
 }
--- src/room-controller/roomController/BTComTask.h
+++ src/room-controller/roomController/BTComTask.h
@@ -2,12 +2,17 @@
 #define __BTCOMTASK__
 
 #include "Task.h"
+#include "ServoMotor.h"
+#include "Led.h"
 
-class OutputTask: public Task {
-
+class BTComTask : public Task {
+  int rxPin;
+  int txPin;
+  ServoMotor* servoMotor;
+  Led* led;
 
 public:
-  OutputTask(int rxPin, int txPin);  
+  BTComTask(int rxPin, int txPin, ServoMotor* servoMotor, Led* led);  
   void init(int period);  
   void tick();
 };
