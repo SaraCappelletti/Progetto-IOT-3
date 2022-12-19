c0ac8cab5ce04b0b934f957df2928fa1525f6c6e
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-12-19 15:02:04 +0100

    Added App in room-app

---
+++ src/room-app/app/src/main/java/com/example/myapplication/kb/C.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.kb;
+
+public class C {
+
+    public class extra {
+        public static final String USERNAME_ID = "username";
+    }
+
+    public class login {
+        public static final String USERNAME = "mario.rossi";
+        public static final String PASSWORD = "test1234";
+    }
+}
