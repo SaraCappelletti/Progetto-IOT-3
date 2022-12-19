2ed36e9a0527c811c6373686e39731966b5287ac
Leonardo Randacio <leonardo.randacio@gmail.com>
2022-12-19 15:15:40 +0100

    added partial roomController implementation

--- src/room-controller/roomController/roomController.ino
+++ src/room-controller/roomController/roomController.ino
@@ -1,25 +1,24 @@
+#include "Const.h"
 #include "Scheduler.h"
 #include "ServoMotor.h"
 #include "Led.h"
+#include "BTComTask.h"
 
 
-
+ServoMotor* servoMotor = new ServoMotor(SERVOMOTOR_PIN);
+Led* led = new Led(LED_PIN);
 Scheduler sched(SCHEDULER_PERIOD);
-
+BTComTask* btComTask = new BTComTask(RX_BT_PIN, TX_BT_PIN);
 
 void setup(){
   Serial.begin(9600);
-  ServoMotor servoMotor(SERVOMOTOR_PIN);
-  Led led(LED_PIN);
+  
   //initializing scheduler
-  sched.init(SCHEDULER_PERIOD);
+  sched.init();
   
   //initializing all tasks and adding them to the scheduler
-  /*
-  monitoringTask->init(MS_NORMAL_PERIOD, data);
-  sched.addTask(monitoringTask);
-  */
-
+  btComTask->init(BT_COM_PERIOD);
+  sched.addTask(btComTask);
 }
 
 void loop(){
