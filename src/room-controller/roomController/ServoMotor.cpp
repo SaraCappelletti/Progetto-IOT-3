7f7609f51d3b4a0b31cacd16944b4eae8d9a6ffd
Leonardo Randacio <leonardo.randacio@gmail.com>
2022-12-22 13:26:05 +0100

    updated the move method in ServoMotor for better incapsulation

--- src/room-controller/roomController/ServoMotor.cpp
+++ src/room-controller/roomController/ServoMotor.cpp
@@ -5,9 +5,11 @@ ServoMotor::ServoMotor(const int pin) : Component(pin) {
 }
 
 void ServoMotor::move(int angle) {
-  //muovo il motore e mi salvo l'ultimo valore dell'angolo
-  motor.write(MIN_PULSE_WIDTH + angle*coeff);
-  this->angle = angle;
+  //moving the motor only if a new angle has been requested
+  if(this->angle != angle){
+    motor.write(MIN_PULSE_WIDTH + angle*coeff);
+    this->angle = angle;    
+  }
 }
 
 int ServoMotor::getAngle() {
