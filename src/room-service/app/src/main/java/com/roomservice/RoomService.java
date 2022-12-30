e18e41244e7cb21166ab88072f807a02f1827410
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2022-12-30 02:52:56 +0100

    Refactor in a new Gradle project

--- src/room-service/app/src/main/java/com/roomservice/RoomService.java
+++ src/room-service/app/src/main/java/com/roomservice/RoomService.java
@@ -1,11 +1,21 @@
 package com.roomservice;
 
+import com.roomservice.scheduler.Scheduler;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 public class RoomService {
     public static void main(String[] args) {
         System.out.println("--------------------\n" +
                             "Room-Service started\n" +
                             "--------------------");
 
+        final Scheduler scheduler = new Scheduler();
 
+        scheduler.start();
+
+//        scheduler.interrupt();
     }
 }
