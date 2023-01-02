51dc7c0f6f7f86b02ad36c9f42a4c4683d1042b2
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-01-03 00:04:08 +0100

    Remove pause between scheduler cycles

--- src/room-service/app/src/main/java/roomservice/scheduler/Scheduler.java
+++ src/room-service/app/src/main/java/roomservice/scheduler/Scheduler.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class Scheduler {
 
-    private Set<Task> taskSet;
+    final private Set<Task> taskSet;
 
     public Scheduler(final Set<Task> taskSet) {
         this.taskSet = new LinkedHashSet<>(taskSet);
@@ -16,12 +16,7 @@ public class Scheduler {
     public void schedule() {
         while (true) {
             this.taskSet.forEach(Task::execute);
-
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
+
 }
