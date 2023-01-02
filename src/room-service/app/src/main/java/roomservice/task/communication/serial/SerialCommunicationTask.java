a29eb01ad23283ebd416b397c62d66fb98d7fbaf
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-01-02 12:47:25 +0100

    Add serial stuff for ping-pong communication

--- src/room-service/app/src/main/java/roomservice/task/communication/serial/SerialCommunicationTask.java
+++ src/room-service/app/src/main/java/roomservice/task/communication/serial/SerialCommunicationTask.java
@@ -1,16 +1,36 @@
-package roomservice.task.communication;
+package roomservice.task.communication.serial;
 
 import roomservice.task.Task;
 
 public class SerialCommunicationTask implements Task {
 
-    public SerialCommunicationTask() {
+    final CommChannel channel;
 
-
+    public SerialCommunicationTask(final String port) throws SerialCommunicationTaskException {
+        try {
+            this.channel = new SerialCommChannel(port, 9600);
+        } catch (Exception e) {
+            throw new SerialCommunicationTaskException();
+        }
     }
+
     @Override
     public void execute() {
-
+        System.out.println("Serial\n" +
+                "--------------------\n");
+        System.out.println("Sending ping");
+        channel.sendMsg("ping");
+        String msg = null;
+        try {
+            msg = this.channel.receiveMsg();
+            System.out.println("Received: "+msg);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (InterruptedException e) {
+        }
     }
 
 }
