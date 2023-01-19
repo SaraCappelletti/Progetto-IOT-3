5e2e5d4c133181ae0bb20744327a0b8783c0d440
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2023-01-19 17:50:37 +0100

    Finished dashboard

--- src/room-dashboard/README.txt
+++ src/room-dashboard/README.txt
@@ -1,1 +1,18 @@
-CIAO
+fare richieste http per chiedere i dati e mandarli se qualcuno li cambia dalla dashboard
+
+type -> "SET | GET"
+if type.value == SET
+            set -> "[[ON | OFF]] / [[0-100]]"
+
+if type.value == GET
+            get -> "
+{
+  data_1: {
+          ora_1: {
+                  luci: xx,
+                  tapp: yy
+          },
+          ...
+  },
+...
+}
---
+++ src/room-dashboard/dashboard.js
@@ -0,0 +1,105 @@
+const lightSwitch = document.getElementById('switch');
+const blindRange = document.getElementById('range');
+const takeControlSwitch = document.getElementById('control');
+
+let dataLight = [];
+const chart = new Chart(document.getElementById('chart'), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Roller blind',
+            data: [],
+            fill: false,
+            borderColor: 'rgb(53, 215, 233)',
+            tension: 0.1
+        }, {
+            data: [],
+            pointRadius: 0,
+            segment: {
+                borderColor: ctx => dataLight[ctx.p0DataIndex] == 'ON' ? 'rgb(53, 233, 82)' : 'rgb(235, 62, 62)',
+            },
+            fill: false,
+            tension: 0
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 18
+                    },
+                    generateLabels: c => [
+                        {
+                            text: 'Roller blind',
+                            fillStyle: 'rgb(53, 215, 233)',
+                        }, {
+                            text: 'Light ON',
+                            fillStyle: 'rgb(53, 233, 82)'
+                        }, {
+                            text: 'Light OFF',
+                            fillStyle: 'rgb(235, 62, 62)'
+                        }
+                    ]
+                }
+            },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                suggestedMin: 0,
+                suggestedMax: 100
+            },
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'second'
+                }
+            }
+        }
+    }
+});
+
+
+setInterval(() => {
+    askForData();
+}, 1000);
+
+fakeData = {};
+function generateFakeData() {
+    fakeData[(new Date()).toString()] = {
+        light: Math.random()<0.5 ? 'ON' : 'OFF',
+        rollerBlind: Math.random()*100|0,
+    };
+    return fakeData;
+}
+
+async function askForData(){
+    let override = new FormData();
+    if (takeControlSwitch.checked) {
+        override.append('light', lightSwitch.checked ? 'ON' : 'OFF');
+        override.append('rollerBlind', +blindRange.value)
+    }
+    /*const resp = await fetch("localhost", {
+        method: 'POST',
+        body: override,
+    });
+    const data = await resp.json();*/
+    const data = generateFakeData();
+
+    updateChart(data);
+}
+
+function updateChart(data) {
+    chart.data.labels = Object.keys(data).map(d => new Date(d));
+    chart.data.datasets[0].data = Object.values(data).map(o => o.rollerBlind);
+    dataLight = Object.values(data).map(o => o.light);
+    chart.data.datasets[1].data = new Array(dataLight.length).fill(0);
+    chart.update();
+}
+
+function takeControl(){
+    lightSwitch.disabled = blindRange.disabled = !takeControlSwitch.checked;
+}
