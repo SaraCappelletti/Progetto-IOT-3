b7b56d8763e95b0006362a748ad506e8c09ee36f
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-01-21 18:05:51 +0100

    (esp) fixed mapping of the value of the photoresistor

--- src/room-sensor-board/roomSensorBoard/Photoresistor.cpp
+++ src/room-sensor-board/roomSensorBoard/Photoresistor.cpp
@@ -7,5 +7,8 @@ Photoresistor::Photoresistor(const int pin) : Sensor(pin) {
 int Photoresistor::read() {
   int value = analogRead(pin);
   value = map(value, 0, 1023, 0, 100);
+  if(value > 100){
+    value = 100;
+  }
   return value;
 }
