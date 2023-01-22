cc4a5c3babfb4ac52dcd8dae62f09384b8b53be1
Leonardo Randacio <leonardo.randacio@gmail.com>
2023-01-22 19:13:05 +0100

    reversed servo motor map

--- src/room-controller/roomController/ServoMotor.cpp
+++ src/room-controller/roomController/ServoMotor.cpp
@@ -6,7 +6,7 @@ ServoMotor::ServoMotor(const int pin, const int tolerance) : Component(pin), tol
 
 void ServoMotor::move(int percentege) {
   //mapping percentege to angle
-  int newAngle = map(percentege, 0, 100, 0, 180);
+  int newAngle = map(percentege, 0, 100, 180, 0);
   //moving the motor only if a new angle has been requested
   //if((newAngle > this->angle + tolerance) || (newAngle < this->angle - tolerance)){
     motor.write(MIN_PULSE_WIDTH + newAngle*coeff);
@@ -19,5 +19,5 @@ int ServoMotor::getAngle() {
 }
 
 int ServoMotor::getPercentege(){
-  return map(this->angle, 0, 180, 0, 100);
+  return map(this->angle, 0, 180, 100, 0);
 }
